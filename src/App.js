a574542ce2a6e6ff95a07ca7b0f4d88402193db9
Dakota Ramey <Dakotaramey6@gmail.com>
2024-01-24 23:54:50 -0500

    Created a dynamic daily change weather

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import { useEffect, useState } from "react";
 function App() {
   const [isLoading, setIsloading] = useState(true);
   const [daily, setDaily] = useState([]);
+  const [active, setActive] = useState("Today's Forecast");
+
+  const handleOnClickbtn = ({ target }) => {
+    setActive(() => target.value);
+  };
 
   useEffect(() => {
     fetch(url)
@@ -12,13 +17,29 @@ function App() {
       .then((json) => {
         setDaily(json.timelines.daily);
         setIsloading(false);
+        console.log(json.timelines);
       })
       .catch((error) => console.error(error));
   }, []);
 
   return (
     <div className="App">
-      {isLoading ? <p>Loading...</p> : <DailyWeather daily={daily} />}
+      {isLoading ? (
+        <Spinner isLoading={isLoading} />
+      ) : (
+        <>
+          <div className="btn-panel">
+            <Buttons onClick={handleOnClickbtn}>Daily Forecast</Buttons>
+            <Buttons onClick={handleOnClickbtn}>Today's Forecast</Buttons>
+          </div>
+
+          {active === "Daily Forecast" ? (
+            <DailyWeather daily={daily} />
+          ) : (
+            <TodaysWeather daily={daily} />
+          )}
+        </>
+      )}
     </div>
   );
 }
@@ -28,7 +49,7 @@ function DailyWeather({ daily }) {
     <ul className="dailyTemps">
       {daily.map((dailys, i) => (
         <li key={i}>
-          <p>{dailys.time.split("T Z")}</p>
+          <p>{dailys.time.split("T")}</p>
           <p>
             Low Temp:{" "}
             {Math.round(
@@ -37,7 +58,7 @@ function DailyWeather({ daily }) {
             °F
           </p>
           <p>
-            Average Temp:{" "}
+            Average Temp:
             {Math.round(
               parseFloat((dailys.values.temperatureAvg * 9) / 5 + 32)
             )}{" "}
@@ -56,6 +77,43 @@ function DailyWeather({ daily }) {
   );
 }
 
-function TodaysTemp() {}
+function TodaysWeather({ daily }) {
+  return (
+    <>
+      <p>
+        Today's Low:{" "}
+        {Math.round(parseFloat((daily[0].values.temperatureMin * 9) / 5 + 32))}{" "}
+        °F
+      </p>
+      <p>
+        Today's Average:{" "}
+        {Math.round(parseFloat((daily[0].values.temperatureAvg * 9) / 5 + 32))}{" "}
+        °F
+      </p>
+      <p>
+        Today's High:{" "}
+        {Math.round(parseFloat((daily[0].values.temperatureMax * 9) / 5 + 32))}{" "}
+        °F
+      </p>
+    </>
+  );
+}
+
+function Buttons(props) {
+  return (
+    <button onClick={props.onClick} value={props.children}>
+      {props.children}
+    </button>
+  );
+}
+
+function Spinner({ isLoading }) {
+  return (
+    <>
+      <p className="loader"></p>
+      <p className="load-menu">Hang Tight! Looking outside at the weather!</p>
+    </>
+  );
+}
 
 export default App;
