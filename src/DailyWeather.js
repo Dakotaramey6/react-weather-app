d0805fcb1012468cd5f01130a68880821271db0b
Dakota Ramey <Dakotaramey6@gmail.com>
2024-01-30 21:45:06 -0500

    Cleaned up code base, added some logic for helper functions

---
+++ src/DailyWeather.js
@@ -0,0 +1,25 @@
+export function DailyWeather({ daily, weatherCalculation, WeatherStatus }) {
+  return (
+    <ul className="dailyTemps">
+      {daily.map((dailys, i) => (
+        <li key={i}>
+          <p>{dailys.time.substring(dailys.time.indexOf("T"), -1)}</p>
+          <p className="weather-icon-daily ">{weatherCalculation(dailys)}</p>
+          <p>{WeatherStatus(WeatherStatus(dailys))}</p>
+          <p></p>
+          <p>
+            {Math.round(
+              parseFloat((dailys.values.temperatureMax * 9) / 5 + 32)
+            )}
+            °/
+            {Math.round(
+              parseFloat((dailys.values.temperatureMin * 9) / 5 + 32)
+            )}
+            °
+          </p>
+          <p>Precipitation {dailys.values.precipitationProbabilityAvg}% </p>
+        </li>
+      ))}
+    </ul>
+  );
+}
